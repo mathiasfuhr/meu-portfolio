4c1e991afffdbe8980ff79414f5b9022ed681be4
mathiasgfuhr <mathiasgilvanf@gmail.com>
2023-10-24 04:02:41 -0300

    certificados

--- script.js
+++ script.js
@@ -32,3 +32,48 @@ document.getElementById("scrollToTopButton").onclick = function () {
     document.body.scrollTop = 0; // Para navegadores mais antigos
     document.documentElement.scrollTop = 0; // Para navegadores modernos
 };
+
+
+
+
+
+
+
+
+function showImage(imageElement) {
+    const modal = document.getElementById("image-modal");
+    const modalImage = document.getElementById("modal-image");
+    
+    modal.style.display = "block";
+    modalImage.src = imageElement.getElementsByTagName("img")[0].src;
+    
+    // Adicione um evento para fechar a imagem quando a tecla "Esc" for pressionada
+    addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            closeImageModal();
+        }
+    });
+}
+
+function closeImageModal() {
+    const modal = document.getElementById("image-modal");
+    modal.style.display = "none";
+    
+    // Remova o evento de fechar a imagem ao pressionar "Esc" ao fechar o modal
+    removeEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            closeImageModal();
+        }
+    });
+}
+
+// Adicione um evento para fechar a imagem ao clicar fora do modal
+addEventListener("click", function(event) {
+    const modal = document.getElementById("image-modal");
+    if (event.target === modal) {
+        closeImageModal();
+    }
+});
+
+
+
